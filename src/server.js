bc434c942cb3e8a9adaf3cdaa3acee2f17b5c7be
Henrique Penha <henrique-dsp34@hotmail.com>
2024-03-26 11:03:38 -0300

    feat: CRUD

--- src/server.js
+++ src/server.js
@@ -11,13 +11,53 @@ app.get('/', (req, res) => {
     res.send('TKTênis_API');
 });
 
-app.post('/users', (req, res) => {
+//CREATE
+app.post('/users', async (req, res) => {
     const { name, email, password } = req.body;
-    const user = new UserService();
-    user.add(name, email, password);
-    res.statusCode = 201;
-    res.json({ message: 'success' })
-})
+    const userService = new UserService();
+    await userService.add(name, email, password);
+    return res.status(201).json({ message: 'Usuário criado com sucesso' })
+});
+
+//READ
+app.get('/users', async (req, res) => {
+    const userService = new UserService();
+    const users = await userService.findAll();
+    return res.status(200).json(users);
+});
+
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const userService = new UserService();
+    const user = await userService.findById(id);
+    return res.status(200).json(user);
+});
+
+//UPDATE
+app.put('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const { name, email, password } = req.body;
+    const user = { name, email, password};
+    const userService = new UserService();
+    try {
+        await userService.update(id, user);
+        return res.status(200).json({ message: 'Atualização realizada com sucesso' });
+    } catch (error) {
+        return res.status(404).json({ message: error.message })
+    }
+});
+
+//DELETE
+app.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const userService = new UserService();
+    try {
+        await userService.delete(id);
+        return res.status(200).json({ message: 'Usuário removido com sucesso' });
+    } catch (error) {
+        return res.status(404).json({ message: error.message })
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
